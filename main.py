57877b475f5650d8218b2404e62b7af3ff17f051
GabrielMB <gabrielmoreirabandeira52@gmail.com>
2026-06-02 11:12:59 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+print('Setup start')
+pygame.init()
+window = pygame.display.set_mode(size =( 800, 600))
+print('Setup end')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
